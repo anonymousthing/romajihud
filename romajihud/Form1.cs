ddd03141acdcd5adf363d18331822b9d05ae6e9d
Sammy Sammon <anonymous-thing@hotmail.com>
2016-12-11 00:17:09 +1000

    Very crappy initial commit

---
+++ romajihud/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace romajihud
+{
+	public partial class Form1 : Form
+	{
+		GlobalHotkey hotkey = new GlobalHotkey();
+
+		public Form1()
+		{
+			InitializeComponent();
+			hotkey.RegisterHotkey(romajihud.ModifierKeys.Control | romajihud.ModifierKeys.Alt, Keys.F12);
+			hotkey.KeyPressed += Hotkey_KeyPressed;
+			this.TopMost = true;
+		}
+
+		private void Hotkey_KeyPressed(object sender, KeyPressedEventArgs e)
+		{
+			if (!Clipboard.ContainsText())
+				return;
+
+			string text = Clipboard.GetText();
+			//string preprocessed = Preprocess(text);
+			string converted = ConvertToRomaji(text);
+			ShowHud(converted);
+		}
+
+		async void ShowHud(string text)
+		{
+			//this.Show();
+			this.Location = Cursor.Position;
+			label1.Text = text;
+			var stringWidth = this.CreateGraphics().MeasureString(text, label1.Font).Width;
+			this.Width = (int)stringWidth + 10;
+			await Task.Run(() =>
+			{
+				Thread.Sleep(5000);
+			});
+			//this.Hide();
+		}
+
+		//Strip out everything other than letters and numbers and change to lowercase
+		string Preprocess(string input)
+		{
+			List<char> result = new List<char>();
+			for (int i = 0; i < input.Length; i++)
+			{
+				if ((input[i] >= '0' && input[i] <= '9') || (input[i] >= 'a' && input[i] <= 'z'))
+				{
+					result.Add(input[i]);
+				}
+				else if (input[i] >= 'A' && input[i] <= 'Z')
+				{
+					result.Add((char)(input[i] | 32));
+				}
+			}
+
+			return string.Join("", result);
+		}
+
+		//Process the ^ and {|} cases, and strip out all other characters other than letters and numbers. Also convert to lowercase.
+		string Postprocess(string input)
+		{
+			List<char> newString = new List<char>();
+			for (int i = 0; i < input.Length; i++)
+			{
+				bool lastSpace = false;
+				if (input[i] == '^' && i > 0)
+				{
+					switch (input[i - 1])
+					{
+						case 'a':
+							newString.Add('a');
+							break;
+						case 'e':
+							newString.Add('e');
+							break;
+						case 'o':
+							newString.Add('u');
+							break;
+
+					}
+				}
+				else if (input[i] == '{' || input[i] == '}' || input[i] == '|' ||
+					(input[i] >= '0' && input[i] <= '9') || (input[i] >= 'a' && input[i] <= 'z'))
+				{
+					lastSpace = false;
+					newString.Add(input[i]);
+				}
+				else if (input[i] >= 'A' && input[i] <= 'Z')
+				{
+					lastSpace = false;
+					newString.Add((char)(input[i] | 32)); //To lower
+				}
+				else
+				{
+					if (!lastSpace)
+					{
+						lastSpace = true;
+						newString.Add(' ');
+					}
+				}
+			}
+
+			return string.Join("", newString);
+		}
+
+		string ConvertToRomaji(string input)
+		{
+			Process process = new Process();
+			process.StartInfo.FileName = @"C:\kakasi\bin\kakasi.exe";
+			process.StartInfo.Arguments = "-s -Ja -Ha -Ka -p -c";
+			process.StartInfo.RedirectStandardOutput = true;
+			process.StartInfo.RedirectStandardInput = true;
+			process.StartInfo.UseShellExecute = false;
+			process.StartInfo.CreateNoWindow = true;
+
+			process.Start();
+
+			StreamWriter writer = new StreamWriter(process.StandardInput.BaseStream, Encoding.GetEncoding(932)); //Codepage 932 = Shift-JIS encoding
+			writer.WriteLine(input);
+			writer.Flush();
+			writer.Close();
+
+			string output = process.StandardOutput.ReadToEnd().Trim();
+
+			return Postprocess(output);
+		}
+	}
+}
---
+++ romajihud/GlobalHotkey.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace romajihud
+{
+	[Flags]
+	enum ModifierKeys : uint
+	{
+		Alt = 1,
+		Control = 2,
+		Shift = 4,
+		Win = 8
+	}
+
+	class KeyPressedEventArgs : EventArgs
+	{
+		public ModifierKeys Modifier;
+		public Keys Key;
+
+		public KeyPressedEventArgs(ModifierKeys modifier, Keys key)
+		{
+			this.Modifier = modifier;
+			this.Key = key;
+		}
+	}
+
+	class Window : NativeWindow, IDisposable
+	{
+		private static int WM_HOTKEY = 0x0312;
+		public event EventHandler<KeyPressedEventArgs> KeyPressed = (s, e) => { };
+
+		public Window()
+		{
+			CreateHandle(new CreateParams());
+		}
+
+		protected override void WndProc(ref Message msg)
+		{
+			base.WndProc(ref msg);
+
+			if (msg.Msg == WM_HOTKEY)
+			{
+				Keys key = (Keys)(((int)msg.LParam >> 16) & 0xFFFF);
+				ModifierKeys modifier = (ModifierKeys)((int)msg.LParam & 0xFFFF);
+
+				KeyPressed(this, new KeyPressedEventArgs(modifier, key));
+			}
+		}
+
+		public void Dispose()
+		{
+			DestroyHandle();
+		}
+	}
+
+	class GlobalHotkey
+	{
+		[DllImport("user32.dll")]
+		private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+		[DllImport("user32.dll")]
+		private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+		private Window window = new Window();
+		private int currentId;
+
+		public event EventHandler<KeyPressedEventArgs> KeyPressed = (s, e) => { };
+
+		public GlobalHotkey()
+		{
+			window.KeyPressed += (s, e) => KeyPressed(this, e);
+		}
+
+		public void RegisterHotkey(ModifierKeys modifier, Keys key)
+		{
+			currentId = currentId + 1;
+
+			if (!RegisterHotKey(window.Handle, currentId, (uint)modifier, (uint)key))
+				throw new InvalidOperationException("Couldn’t register the hot key.");
+		}
+
+		public void Dispose()
+		{
+			for (int i = currentId; i > 0; i--)
+				UnregisterHotKey(window.Handle, i);
+			
+			window.Dispose();
+		}
+	}
+}
